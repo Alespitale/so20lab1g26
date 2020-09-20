eaebd71a32696035dedddafadd841b71154f22bd
Ale <alespitale6@gmail.com>
2020-09-20 20:26:15 -0300

    actualizacion de execute y bultin

--- builtin.c
+++ builtin.c
@@ -1,25 +1,67 @@
-#include <string.h>
 #include "builtin.h"
+#include "strextra.h"
 #include <assert.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "tests/syscall_mock.h"
 
 bool builtin_is_internal(scommand cmd){
     assert(cmd != NULL);
-    int a = strcmp(scommand_front,"cd");
-    int b = strcmp(scommand_front,"exit");
+    int a = strcmp(scommand_front(cmd),"cd");
+    int b = strcmp(scommand_front(cmd),"exit");
     return (a == 0 || b == 0);
 }
 
 void builtin_exec(scommand cmd){
     assert(builtin_is_internal(cmd));
-    if (strcmp(scommand_front,"cd") == 0){
+    if (strcmp(scommand_front(cmd),"cd") == 0){
         scommand_pop_front(cmd);
-        if(chdir(scommand_front(cmd)) != 0){
-            printf("chdir to %s failed \n",scommand_front(cmd));
+        int ret = chdir(scommand_front(cmd));
+        
+        if(ret != 0){
+            switch (ret)
+            {
+            case EFAULT: 
+                perror("path apunta fuera de su espacio de direcciones accesible.");
+                break;
+            case ENAMETOOLONG:
+                perror("path es demasiado largo.");
+                break;
+            case ENOENT:
+                perror("El fichero no existe.");
+                break;
+            case ENOMEM:
+                perror("No hay suficiente memoria disponible en el núcleo.");
+                break;
+            case ENOTDIR:
+                perror("Un componente del camino path no es un directorio.");
+                break;
+            case EACCES:
+                perror("Ha sido denegado el permiso de búsqueda en uno de los componentes del camino path.");
+                break;
+            case ELOOP:
+                perror("Se han encontrado demasiados enlaces simbólicos al resolver path");
+                break;
+            case EIO:
+                perror("Ha ocurrido un error de E/S.");
+                break;
+            default:
+                perror("No se pudo cambiar de directorio");
+                break;
+            }
+            cmd = scommand_destroy(cmd);
+            exit(0);
         }
+        
     }else {
         scommand_pop_front(cmd);
         cmd = scommand_destroy(cmd);
         exit(0);
     }
 } 
+
+
--- execute.c
+++ execute.c
@@ -5,64 +5,168 @@
 #include <sys/wait.h>
 #include <gmodule.h>
 #include <stdio.h>
-#include <sys/stat.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/stat.h> 
 #include <fcntl.h>
+#include "tests/syscall_mock.h"
+
+static char ** scommand_adapted(scommand self){
+    char **argv = calloc(scommand_length(self),sizeof(char *));
+    for (guint i = 0 ; i < scommand_length(self) ; i++){
+        argv[i] = strdup(scommand_front(self));
+        scommand_pop_front(self);
+    }
+    return argv;
+}
 
 
+static void check_fd_out(int fd){
+    if (fd == -1){
+        perror("fallo mock_open");
+        exit(0);
+    }
+    if(dup2(fd,1) == -1){
+        perror("fallo dup");
+        exit(0);
+    }
+    if(close(fd) == -1){
+        perror("fallo close");
+        exit(0);
+    }
+}
+static void check_fd_in(int fd){
+    if (fd == -1){
+        perror("fallo mock_open");
+        exit(0);
+    }
+    if(dup2(fd,0) == -1){
+        perror("fallo dup");
+        exit(0);
+    }
+    if(close(fd) == -1){
+        perror("fallo close");
+        exit(0);
+    }
+}
 
 void execute_pipeline(pipeline apipe){
+    assert(!pipeline_is_empty(apipe));
     unsigned int N = pipeline_length(apipe);
-    if(N == 1){                             // Es un comando simple
+    if(N == 1){             //Es un comando simple
         if(builtin_is_internal(pipeline_front(apipe))){
             builtin_exec(pipeline_front(apipe));
-            apipe = pipeline_destroy(apipe);
-        }else{
+            //apipe = pipeline_destroy(apipe);
+        }
+    }else{
+            char ** cmd_adapted = scommand_adapted(pipeline_front(apipe));
+           // for(guint i = 0; i < cmd_adapted[i]; i++){
+           //     strdup(cmd_adapted[i]);
+           // }
             pid_t pid = fork();
-            if (pid < 0){                   // falló fork; exit 
+            if (pid < 0){           // fork failed; exit
                 fprintf(stderr, "fork failed\n");
                 exit(1);
-            }else if (pid == 0){            // proceso hijo (nuevo proceso)
-                scommand sc = pipeline_front(apipe);
-                guint L = scommand_length(sc);               
-                //char * arg = calloc(L+1,sizeof(char));
-                char * arg[L];
-                char * dir_in = scommand_get_redir_in(sc);
-                char * dir_out = scommand_get_redir_out(sc);
-                if(dir_in != NULL){
-                    close (0);
-                    open(dir_in,O_CREAT|O_WRONLY|O_TRUNC);
-                }
-                for (guint i = 0; i < L; i++){
-                    arg[i] = scommand_front(sc);
-                    scommand_pop_front(sc);
-                }
-                sc = scommand_destroy(sc);
-                arg[L] = NULL;
-                execvp(arg[0], arg);  
-                exit(1);                     // asegura que no sigan procesos 
-            }else{                           // (pid > 0) proceso padre
-                if(pipeline_get_wait(apipe)){
+            }else if ( pid > 0){    // padre
+                if (pipeline_get_wait(apipe)){
                     wait(NULL);
+                }else{
+                    //for (guint i=0 ; i < cmd_adapted[i]; i++){
+                    //    g_free(cmd_adapted[i]);
+                   //}
+                    //g_free(cmd_adapted);
+                }
+            }else {   // hijo
+                if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) == NULL){
+                    execvp(cmd_adapted[0],cmd_adapted);
+                }else if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) != NULL){
+                    int fd = open(scommand_get_redir_out(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
+                    check_fd_out(fd);
+                    execvp(cmd_adapted[0],cmd_adapted);
+                }else if(scommand_get_redir_in(pipeline_front(apipe)) != NULL && scommand_get_redir_out(pipeline_front(apipe)) == NULL){
+                    int fd = open(scommand_get_redir_in(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
+                    check_fd_in(fd);
+                    execvp(cmd_adapted[0],cmd_adapted);
+                }else{
+                    int fd1 = open(scommand_get_redir_in(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
+                    check_fd_in(fd1);
+                    int fd = open(scommand_get_redir_out(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
+                    check_fd_out(fd);
+                    execvp(cmd_adapted[0],cmd_adapted);                    
                 }
             }
-        }
-    }else{                                  // Hay mas de un comando
-        for (guint i =0; i < N; i++){
-            int pipes[2];                   //uno para el extremo de escritura y otro para el extremo de lectura
-            pipe (pipes);
-            pid_t pid = fork();
-            scommand sc = pipeline_front(apipe);
-            if(pid < 0){                    //error
-
-            }
-            else if(pid == 0){              // hijo
-
-            }
-            else{                           //padre
-
-            } 
-        }
     }
 }
 
 
+
+
+
+/*
+char ** scommand_adapted(scommand self){
+    char **argv = calloc(scommand_length(self),sizeof(char *));
+    for (guint i = 0 ; i < scommand_length(self) ; i++){
+        argv[i] = strdup(scommand_front(self));
+        scommand_pop_front(self);
+    }
+    return argv;
+}
+
+void execute_pipeline(pipeline apipe){
+    assert(apipe != NULL);
+    unsigned int N = pipeline_length(apipe);
+    if(N == 1){             //Es un comando simple
+        if(builtin_is_internal(pipeline_front(apipe))){
+            builtin_exec(pipeline_front(apipe));
+            //apipe = pipeline_destroy(apipe);
+        }else{
+            pid_t pid = fork();
+            if (pid < 0){           // fork failed; exit
+                fprintf(stderr, "fork failed\n");
+                exit(1);
+            }else if ( pid > 0){    // padre
+                if (pipeline_get_wait(apipe)){
+                    wait(NULL);
+                   // for (guint i=0 ; i < cmd_adapted[i]; i++){
+                   //    g_free(cmd_adapted[i]);
+                   // }
+                   // g_free(cmd_adapted);
+                }
+            }else if ( pid == 0){   // hijo
+                char ** cmd_adapted = scommand_adapted(pipeline_front(apipe));
+               // for(guint i = 0; i < cmd_adapted[i]; i++){
+               //     strdup(cmd_adapted[i]);
+              //  }
+                if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) == NULL){
+                    execvp(cmd_adapted[0],cmd_adapted);
+                }else if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) != NULL){
+                    int fd = mock_open(scommand_get_redir_in(pipeline_front(apipe)),O_CREAT|O_WRONLY|O_TRUNC);
+                    if (fd == -1){
+                        perror("fallo mock_open");
+                        exit(2);
+                    }
+                    if(dup2(fd,0) == -1){
+                        perror("fallo dup");
+                        exit(2);
+                    }
+                    if(close(fd)== -1){
+                        perror("fallo close");
+                    }
+                    int fd1 = mock_open(scommand_get_redir_in(pipeline_front(apipe)),O_CREAT|O_WRONLY|O_TRUNC);
+                    if (fd1 == -1){
+                        perror("fallo mock_open");
+                        exit(2);
+                    }
+                    if(dup2(fd1,1) == -1){
+                        perror("fallo dup");
+                    }
+                    if(close(fd1)== -1){
+                        perror("fallo close");
+                    }
+                    execvp(cmd_adapted[0],cmd_adapted);
+                }
+            }
+        }
+    }
+}
+*/
