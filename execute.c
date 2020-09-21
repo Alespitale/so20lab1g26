73165061e229cfd04fa5f1b90823d30871688c84
Ale <alespitale6@gmail.com>
2020-09-21 17:14:52 -0300

    falta agregar el caso donde no espera los comandos

--- execute.c
+++ execute.c
@@ -10,6 +10,8 @@
 #include <sys/stat.h> 
 #include <fcntl.h>
 #include "tests/syscall_mock.h"
+#define READ 0
+#define WRITE 1
 
 static char ** scommand_adapted(scommand self){
     char **argv = calloc(scommand_length(self)+1,sizeof(char *));
@@ -21,7 +23,6 @@ static char ** scommand_adapted(scommand self){
         argv[(len)]= NULL;
     return argv;
 }
-
 
 static void check_fd_out(int fd){
     if (fd == -1){
@@ -37,6 +38,7 @@ static void check_fd_out(int fd){
         exit(0);
     }
 }
+
 static void check_fd_in(int fd){
     if (fd == -1){
         perror("fallo mock_open");
@@ -52,93 +54,122 @@ static void check_fd_in(int fd){
     }
 }
 
+static void ex_cmd(pipeline apipe){     //Ejecuta comando interno o externo sin pipes
+    if(builtin_is_internal(pipeline_front(apipe))){
+        builtin_exec(pipeline_front(apipe));
+    }else{
+        char ** cmd_adapted = scommand_adapted(pipeline_front(apipe));
+        pid_t pid = fork();
+        if (pid < 0){           // fork failed; exit
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        }else if ( pid > 0){    // padre
+            if (pipeline_get_wait(apipe)){
+                wait(NULL);
+            }else{
+                    // esto da leak
+            }
+        }else{     // hijo
+            char *in = scommand_get_redir_in(pipeline_front(apipe));
+            char *out = scommand_get_redir_out(pipeline_front(apipe));
+            if ( in == NULL && out == NULL){
+                execvp(cmd_adapted[0],cmd_adapted);
+            
+            }else if (in == NULL && out != NULL){
+                int fd = open(out, O_CREAT|O_WRONLY,S_IWUSR);
+                check_fd_out(fd);
+                execvp(cmd_adapted[0],cmd_adapted);
+            
+            }else if(in != NULL && out == NULL){
+                int fd = open(in, O_CREAT|O_RDONLY,S_IRUSR);
+                check_fd_in(fd); 
+                execvp(cmd_adapted[0],cmd_adapted);
+            
+            }else{
+                int fd1 = open(in, O_CREAT|O_RDONLY ,S_IRUSR);
+                check_fd_in(fd1);
+                int fd = open(out, O_CREAT|O_WRONLY,S_IWUSR);
+                check_fd_out(fd);
+                execvp(cmd_adapted[0],cmd_adapted);                    
+            }
+        }   
+    }
+}
+
 void execute_pipeline(pipeline apipe){
     assert(apipe != NULL);
     unsigned int N = pipeline_length(apipe);
-    if(N == 0){             //es comando vacio
+    int state;
+    if(N == 1){             // Solo un comando
+        ex_cmd(apipe);
     }
-    if(N == 1){             //Es un comando simple
-        if(builtin_is_internal(pipeline_front(apipe))){            // Es comando interno
-            builtin_exec(pipeline_front(apipe));
-            //apipe = pipeline_destroy(apipe);
+    else if (N >= 2){      // Al menos 2 comandos usamos pipes
+        int ** fd = calloc(N , sizeof(int *));
+        for ( guint i = 0 ; i < N ; i++){
+            fd[i] = calloc(2 , sizeof(int));
         }
-        else{                                                      // Es comando externo
-            char ** cmd_adapted = scommand_adapted(pipeline_front(apipe));
-            pid_t pid = fork();
-            if (pid < 0){           // fork failed; exit
-                fprintf(stderr, "fork failed\n");
-                exit(1);
-            }else if ( pid > 0){    // padre
-                if (pipeline_get_wait(apipe)){
-                    wait(NULL);
-                }else{
-                        // esto da leak
-                }
-            }else{     // hijo
-                if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) == NULL){
-                    execvp(cmd_adapted[0],cmd_adapted);
-                }else if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) != NULL){
-                    int fd = open(scommand_get_redir_out(pipeline_front(apipe)),O_CREAT|O_WRONLY,0);
-                    check_fd_out(fd);
-                    execvp(cmd_adapted[0],cmd_adapted);
-                }else if(scommand_get_redir_in(pipeline_front(apipe)) != NULL && scommand_get_redir_out(pipeline_front(apipe)) == NULL){
-                    int fd = open(scommand_get_redir_in(pipeline_front(apipe)),O_CREAT|O_RDONLY,S_IRUSR);
-                    check_fd_in(fd);
-                    execvp(cmd_adapted[0],cmd_adapted);
-                }else{
-                    int fd1 = open(scommand_get_redir_in(pipeline_front(apipe)),O_CREAT|O_RDONLY ,S_IRUSR);
-                    check_fd_in(fd1);
-                    int fd = open(scommand_get_redir_out(pipeline_front(apipe)),O_CREAT|O_WRONLY,S_IWUSR);
-                    check_fd_out(fd);
-                    execvp(cmd_adapted[0],cmd_adapted);                    
-                }
-            } 
+        pid_t pid;
+        GQueue * adapted_commands = g_queue_new();
+        for (unsigned int i = 0 ; i < N ; i++){
+            g_queue_push_tail(adapted_commands,scommand_adapted(pipeline_front(apipe)));
+            pipeline_pop_front(apipe);
         }
-    }
-}   
-/*
-    if(N >=1){             //Es un comando simple
-            for (guint i= 0; i<N; i++){
-                if(builtin_is_internal(pipeline_front(apipe))){            // Es comando interno
-                builtin_exec(pipeline_front(apipe));
-                }else{
-                    pipe()
-                }
-                //apipe = pipeline_destroy(apipe);
-            }
-            }
-        
-            else{                                                      // Es comando externo
-                char ** cmd_adapted = scommand_adapted(pipeline_front(apipe));
-                pid_t pid = fork();
-                if (pid < 0){           // fork failed; exit
+        for (unsigned int i = 0 ; i < N ; i++){
+            char ** cmd = g_queue_pop_head(adapted_commands);
+            if (i == 0){                    // Primer comando solo hace write sobre el pipe
+                pipe(fd[i]);
+                pid = fork();
+                if(pid < 0){
                     fprintf(stderr, "fork failed\n");
                     exit(1);
-                }else if ( pid > 0){    // padre
-                    if (pipeline_get_wait(apipe)){
-                        wait(NULL);
-                    }else{
-                            // esto da leak
-                    }
-                }else {     // hijo
-                    if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) == NULL){
-                        execvp(cmd_adapted[0],cmd_adapted);
-                    }else if (scommand_get_redir_in(pipeline_front(apipe)) == NULL && scommand_get_redir_out(pipeline_front(apipe)) != NULL){
-                        int fd = open(scommand_get_redir_out(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
-                        check_fd_out(fd);
-                        execvp(cmd_adapted[0],cmd_adapted);
-                    }else if(scommand_get_redir_in(pipeline_front(apipe)) != NULL && scommand_get_redir_out(pipeline_front(apipe)) == NULL){
-                        int fd = open(scommand_get_redir_in(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
-                        check_fd_in(fd);
-                        execvp(cmd_adapted[0],cmd_adapted);
-                    }else{
-                        int fd1 = open(scommand_get_redir_in(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
-                        check_fd_in(fd1);
-                        int fd = open(scommand_get_redir_out(pipeline_front(apipe)),0,O_CREAT|O_WRONLY|O_TRUNC);
-                        check_fd_out(fd);
-                        execvp(cmd_adapted[0],cmd_adapted);                    
-                    }
-                } 
+                }else if (pid == 0){                     // hijo
+                    close(fd[i][READ]);                  // cerramos entrada de lectura
+                    dup2(fd[i][WRITE],STDOUT_FILENO);    // cambiamos el out
+                    close(fd[i][WRITE]);                 // cerramos la escritura
+                    execvp(cmd[0],cmd);
+                }else if (pid > 0){             //padre
+                   close(fd[i][WRITE]);
+                }
+            }else if (i > 0 && i != (N-1)){    //Hacen read and write sobre el pipe
+                pipe(fd[i]);
+                pid = fork();
+                if(pid < 0){        //fallo fork
+                    fprintf(stderr, "fork failed\n");
+                    exit(1);
+                }else if (pid == 0){        //hijo 
+                    close(fd[i-1][WRITE]);
+                    dup2(fd[i-1][READ],STDIN_FILENO);
+                    close(fd[i-1][READ]);
+                    close(fd[i][READ]);
+                    dup2(fd[i][WRITE],STDOUT_FILENO);
+                    close(fd[i][WRITE]);
+                    execvp(cmd[0],cmd);
+                }else if(pid > 0){  // padre
+                    close(fd[i-1][READ]);
+                    close(fd[i][WRITE]);
+                }
+            }else if(i == N-1){        // Ultimo comando solo hace read del pipe
+                pid = fork();
+                if ( pid < 0) {
+                    fprintf(stderr, "fork failed\n");
+                    exit(1);
+                  }else if (pid == 0){
+                    close(fd[i-1][WRITE]);
+                    dup2(fd[i-1][READ],STDIN_FILENO);
+                    close(fd[i-1][READ]);
+                    execvp(cmd[0],cmd);
+                }else if (pid > 0){
+                    close(fd[i-1][READ]);
+                }
             }
-   }
-}*/ 
+        }        
+        for(guint i = 0; i < N; i++){ // espera a que terminen los procesos
+            wait(&state);
+        }
+        for(guint i=0; i < N; i++){     // libero los pipe
+            free(fd[i]);
+        }
+        free(fd);
+    }
+}
+
